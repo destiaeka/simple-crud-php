96b9573b564af3e618c786d85ea55d85c472f843
destiaeka <163701104+destiaeka@users.noreply.github.com>
2025-09-24 09:33:15 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -8,6 +8,6 @@
     3. Password, sedangkan untuk passwordnya adalah kosong secara default.
     4. Nama databases, disini saya membuat database bernama db_crud sebagai contoh. Jika teman-teman menggunakan database lain, sesuaikan juga nama database disini, nama table dan field yang ada dalam kodingan folder ini. Baik dalam file index.php maupun file lainya, yang membutuhkan field table teman-teman.
   */
-  $koneksi = mysqli_connect('localhost', 'root', '', 'db_crud');
+  $koneksi = mysqli_connect('localhost', 'crd', '123', 'crud_db');
 
 ?>
